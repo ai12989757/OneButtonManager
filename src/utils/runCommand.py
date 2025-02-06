db75dfa0e8d469d2e6eb9f664b895a8cee3cc152
一百 <159689757@qq.com>
2025-02-06 16:48:50 +0800

    修复了一个bug

--- src/utils/runCommand.py
+++ src/utils/runCommand.py
@@ -2,35 +2,21 @@
 from maya import mel
 import maya.cmds as cmds
 
+def exec_command(command, context):
+    exec(command, {}, context)
 
-# 兼容 Python 2 和 3 的 exec 函数
-def exec_(code, globals=None, locals=None):
-    if globals is None:
-        globals = {}
-    globals.update({
-        '__builtins__': __builtins__,
-    })
-    exec(code, globals, locals)
+def exec_mel_command(command):
+    exec(command)
 
 def runCommand(widget, command, trigger='click'):
-    
     if widget.type == 'QWidget':
-    #if hasattr(widget, 'mouseState'):
-        # if widget.mouseState in ['leftPress','leftRelease'] and trigger not in ['leftPress','leftRelease']:
-        #     if widget.mouseState == 'leftPress':
-        #         cmds.undoInfo(openChunk=True)
-        #     if widget.mouseState == 'leftRelease':
-        #         cmds.undoInfo(closeChunk=True)
-        # else:
         if not command: return
         if trigger not in command: return
         if trigger not in command.keys(): return
         if widget.type == 'QWidget':
-            #print(command[trigger][1])
-            
             if command[trigger][1] == '' or command[trigger][1] is None: return
             if command[trigger][0] == 'python': 
-                cmds.evalDeferred(lambda: exec_(command[trigger][1], widget.context))
+                cmds.evalDeferred(lambda: exec_command(command[trigger][1], widget.context))
             elif command[trigger][0] == 'mel':
                 commendText = repr(command[trigger][1])
                 commendText = "mel.eval(" + commendText + ")"
@@ -38,7 +24,7 @@ def runCommand(widget, command, trigger='click'):
                     mel.eval("string $mouseState=\""+str(widget.mouseState)+"\";")
                     mel.eval("int $deltaX="+str(widget.delta.x())+";")
                     mel.eval("int $deltaY="+str(widget.delta.y())+";")
-                exec_(commendText)
+                exec_mel_command(commendText)
             elif command[trigger][0] == 'function': 
                 command[trigger][1]()
     elif widget.type == 'QAction':
@@ -48,13 +34,13 @@ def runCommand(widget, command, trigger='click'):
         if command[trigger][1] == '' or command[trigger][1] is None: return
         if command[trigger][0] == 'python': 
             cmds.undoInfo(openChunk=True)
-            cmds.evalDeferred(lambda: exec_(command[trigger][1], widget.context))
+            cmds.evalDeferred(lambda: exec_command(command[trigger][1], widget.context))
             cmds.undoInfo(closeChunk=True)
         elif command[trigger][0] == 'mel':
             commendText = repr(command[trigger][1])
             commendText = "mel.eval(" + commendText + ")"
             cmds.undoInfo(openChunk=True)
-            exec_(commendText)
+            exec_mel_command(commendText)
             cmds.undoInfo(closeChunk=True)
         elif command[trigger][0] == 'function': 
             cmds.undoInfo(openChunk=True)
