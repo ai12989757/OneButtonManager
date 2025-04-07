ddf9a8eb8300e1433fee963c738dd201f327f839
一百 <159689757@qq.com>
2025-04-07 15:03:17 +0800

    输出

--- src/utils/changeCommandLineBGC.py
+++ src/utils/changeCommandLineBGC.py
@@ -9,6 +9,7 @@ except ImportError:
     from PySide2.QtWidgets import *
 # -*- coding: utf-8 -*-
 import maya.OpenMayaUI as omui
+from maya import cmds
 try:
     from shiboken6 import wrapInstance
 except ImportError:
@@ -33,23 +34,52 @@ class ChangeCommandLineBGC():
     '''
     maya打开时自动加载了
     commandLineBG = changeCommandLineBGC.ChangeCommandLineBGC()
-    调用时直接使用 commandLineBG.setBGColor(color='#ff5a5a', flicker=True)
+    调用时直接使用 commandLineBG.setBGColor('#ff5a5a', flicker=True)
     '''
     def __init__(self):
+        
         '''
         初始化得到命令行输出控件 QLineEdit
         连接 textChanged 信号，当文本中包含特定字符时改变背景颜色
         '''
+        self.outMessageUI = None
+        self.init()
+
+    # 初始化
+    def init(self):
+        for widget in QApplication.topLevelWidgets():
+            if widget.objectName() == 'outMessageUI':
+                self.outMessageUI = widget
+                return
+            
+        if not self.outMessageUI:
+            self.set_outMessageUI()
+
+    def set_outMessageUI(self):
+        maya_version = cmds.about(version=True)
         self.commandLineUI = mayaToQT('commandLine1')
         self.outMessageUI = None
         # 获取 commandLineUI 下的所有子控件
         for child in self.commandLineUI.children():
-            # 如果是 QLineEdit 类型的控件
-            if isinstance(child, QLineEdit):
-                # 如果 line 是可编辑的
-                if child.isReadOnly():
-                    self.outMessageUI = child
-                    break
+            if maya_version >= '2025':
+                if isinstance(child, QWidget):
+                    for i in child.children():
+                        if isinstance(i, QLineEdit):
+                            # 如果 line 是可编辑的
+                            if i.isReadOnly():
+                                self.outMessageUI = i
+                                break
+            else:
+                # 如果是 QLineEdit 类型的控件
+                if isinstance(child, QLineEdit):
+                    # 如果 line 是可编辑的
+                    if child.isReadOnly():
+                        self.outMessageUI = child
+                        break
+        if not self.outMessageUI:
+            return
+        self.outMessageUI.setObjectName('outMessageUI')
+        self.outMessageUI.textChanged.connect(None)
         self.outMessageUI.textChanged.connect(self.textChangedChangeBGColor)
 
     def flickerBG(self, color, times=2):
@@ -88,16 +118,43 @@ class ChangeCommandLineBGC():
         根据文本内容改变背景颜色
         '''
         text = self.outMessageUI.text()
-        if text.startswith('// Error: ') or text.startswith(u'// 错误: ') or text.startswith('\n// Error: ') or text.startswith(u'\n// 错误: '):
-            self.setBGColor('#ff5a5a')
-        elif text.startswith('// Warning: ') or text.startswith(u'// 警告: ') or text.startswith('\n// Warning: ') or text.startswith(u'\n// 警告: '):
-            self.setBGColor('#dcce87')
-        elif text.startswith('// Info: ') or text.startswith(u'// 信息: ') or text.startswith('\n// Info: ') or text.startswith(u'\n// 信息: '):
-            self.setBGColor('#00BFFF')
-        elif(
-            text.startswith('// Success: ') or text.startswith('\n// Success: ') or text.startswith(u'// 成功: ') or text.startswith(u'\n// 成功: ')
-            or text.startswith('// Finished: ') or text.startswith('\n// Finished: ') or text.startswith(u'// 完成: ') or text.startswith(u'\n// 完成: ')
-            ):
-            self.setBGColor('#5aff5a')
+        if text:
+            if text.startswith('// Error: ') or text.startswith(u'// 错误: ') or text.startswith('\n// Error: ') or text.startswith(u'\n// 错误: '):
+                self.setBGColor('#ff5a5a')
+            elif text.startswith('// Warning: ') or text.startswith(u'// 警告: ') or text.startswith('\n// Warning: ') or text.startswith(u'\n// 警告: '):
+                self.setBGColor('#dcce87')
+            elif text.startswith('// Info: ') or text.startswith(u'// 信息: ') or text.startswith('\n// Info: ') or text.startswith(u'\n// 信息: '):
+                self.setBGColor('#00BFFF')
+            elif(
+                text.startswith('// Success: ') or text.startswith('\n// Success: ') or text.startswith(u'// 成功: ') or text.startswith(u'\n// 成功: ')
+                or text.startswith('// Finished: ') or text.startswith('\n// Finished: ') or text.startswith(u'// 完成: ') or text.startswith(u'\n// 完成: ')
+                ):
+                self.setBGColor('#5aff5a')
+            else:
+                self.outMessageUI.setStyleSheet('')
         else:
             self.outMessageUI.setStyleSheet('')
+
+    def OnePrint(self, message, color=None, flicker=False):
+        '''
+        一次性打印信息
+        args:
+            message: str 信息
+            color: str 颜色 '#ff5a5a' or 'red' or 'rgb(255, 90, 90)' or 'rgba(255, 90, 90, 255)'
+        '''
+        print(message)
+        self.outMessageUI.setText(message)
+        if color:
+            self.setBGColor(color)
+        if flicker:
+            if not color:
+                if message.startswith('// Error: ') or message.startswith(u'// 错误: ') or message.startswith('\n// Error: ') or message.startswith(u'\n// 错误: '):
+                    color = '#ff5a5a'
+                elif message.startswith('// Warning: ') or message.startswith(u'// 警告: ') or message.startswith('\n// Warning: ') or message.startswith(u'\n// 警告: '):
+                    color = '#dcce87'
+                elif message.startswith('// Info: ') or message.startswith(u'// 信息: ') or message.startswith('\n// Info: ') or message.startswith(u'\n// 信息: '):
+                    color = '#00BFFF'
+                elif message.startswith('// Success: ') or message.startswith('\n// Success: ') or message.startswith(u'// 成功: ') or message.startswith(u'\n// 成功: '):
+                    color = '#5aff5a'
+            if color:
+                self.flickerBG(color)
